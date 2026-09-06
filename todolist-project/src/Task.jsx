b3ca50989b615908773ddb5a8723d5c3af13aeb5
mhmdnyb <106429754+mhmdnyb@users.noreply.github.com>
2026-09-06 23:58:12 +0330

    change ui of task componenet

--- todolist-project/src/Task.jsx
+++ todolist-project/src/Task.jsx
@@ -2,7 +2,7 @@ function Task({ title }) {
   return (
     <label htmlFor={title} className="group cursor-pointer">
       <input type="checkbox" name={title} id={title} className="sr-only" />
-      <div className="group-has-checked:bg-fuchsia-700 bg-fuchsia-500 opacity-80 backdrop-blur-2xl transition-colors w-95 h-fit rounded-md border-2 border-white/50 py-3 px-1.5">
+      <div className="group-has-checked:bg-purple-700 bg-purple-500 transition-colors w-95 h-fit rounded-md border-2 border-white/50 py-3 px-1.5">
         <h1 className="text-2xl font-bold capitalize">{title}</h1>
       </div>
     </label>
