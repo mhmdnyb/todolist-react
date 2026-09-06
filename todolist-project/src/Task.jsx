b5caf90a7ead1e214034ab814c8d0b3bc4f57187
mhmdnyb <106429754+mhmdnyb@users.noreply.github.com>
2026-09-06 23:56:25 +0330

    add improvement to task componenet

--- todolist-project/src/Task.jsx
+++ todolist-project/src/Task.jsx
@@ -1,9 +1,9 @@
 function Task({ title }) {
   return (
-    <label htmlFor={title} className="group w-95 h-30 cursor-pointer">
+    <label htmlFor={title} className="group cursor-pointer">
       <input type="checkbox" name={title} id={title} className="sr-only" />
-      <div className="group-has-checked:bg-fuchsia-700 bg-fuchsia-500 opacity-80 backdrop-blur-2xl transition-colors w-95 h-30 rounded-md border-2 border-white/50">
-        <h1>{title}</h1>
+      <div className="group-has-checked:bg-fuchsia-700 bg-fuchsia-500 opacity-80 backdrop-blur-2xl transition-colors w-95 h-fit rounded-md border-2 border-white/50 py-3 px-1.5">
+        <h1 className="text-2xl font-bold capitalize">{title}</h1>
       </div>
     </label>
   );
